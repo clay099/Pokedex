bd2d6dca9520558a904c2fb4ad42176fd7aa5c05
Clayton Whittaker <clayton.whittaker@gmail.com>
2020-08-16 16:27:52 -0700

    added styling

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,15 +2,24 @@ import React from "react";
 import Pokecard from "./Pokecard";
 import "./Pokedex.css";
 
-const Pokedex = ({ list, isWinner, totalExp }) => (
-	<>
-		<h1 className="Pokedex-title">Pokedex</h1>
-		{list.map((p) => (
-			<Pokecard id={p.id} name={p.name} type={p.type} base_experience={p.base_experience} />
-		))}
-		<h3 className="Pokedex-total">Total Experience: {totalExp}</h3>
-		<h3 className="Pokedex-isWinner">{isWinner === true ? "THIS HAND WINS" : ""}</h3>
-	</>
-);
+const Pokedex = ({ list, isWinner, totalExp }) => {
+	let BGcolor = { true: "lightGreen", false: "white" };
+	return (
+		<div style={{ backgroundColor: BGcolor[isWinner] }} className="Pokedex">
+			<h1 className="Pokedex-title">Pokedex Group</h1>
+			{list.map((p) => (
+				<Pokecard
+					key={p.id}
+					id={p.id}
+					name={p.name}
+					type={p.type}
+					base_experience={p.base_experience}
+				/>
+			))}
+			<h3 className="Pokedex-total">Total Experience: {totalExp}</h3>
+			<h3 className="Pokedex-isWinner">{isWinner === true ? "THIS HAND WINS" : ""}</h3>
+		</div>
+	);
+};
 
 export default Pokedex;
